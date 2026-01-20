31b3587066e090963c1ae0f3c39a2d87bc8646e3
EK-DCU-ECE <eoin270@gmail.com>
2026-01-20 12:39:49 +0000

    Wifi Testing
    Successful upon hotspot connection

---
+++ wifi_testing.ino
@@ -0,0 +1,30 @@
+#include <WiFi.h>
+
+const char* ssid = "YOUR_SSID";
+const char* password = "YOUR_PASSWORD";
+
+void setup() {
+  Serial.begin(9600);
+  delay(1000);
+  Serial.println("Connecting to Wi-Fi...");
+
+  WiFi.begin(ssid, password);
+  int attempts = 0;
+  while (WiFi.status() != WL_CONNECTED && attempts < 30) {
+    Serial.print(".");
+    delay(500);
+    attempts++;
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("\nConnected!");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+  } else {
+    Serial.println("\nFailed to connect to Wi-Fi.");
+  }
+}
+
+void loop() {}
+
+
